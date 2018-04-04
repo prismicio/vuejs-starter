1a58c11531028211bbc3d8c92c8078acc3e4450d
jbdebiasio <jbaptistedebiasio@gmail.com>
2018-04-04 17:16:42 +0200

    review steps 1 and 5 content

--- src/components/Tutorial.vue
+++ src/components/Tutorial.vue
@@ -9,7 +9,7 @@
       <div class="wrapper">
         <img src="@/assets/img/tutorial/rocket.svg" alt="Rocket">
         <h1>High five, you deserve it!</h1>
-        <p>Grab a well deserved cup of coffee, you're just a few steps away from creating a page with dynamic content.</p>
+        <p>Grab a well deserved cup of coffee, you’re just a few steps away from creating a page with dynamic content.</p>
       </div>
       <div class="hero-curve"></div>
       <div class="flip-flap">
@@ -30,25 +30,32 @@
 
     <section>
 
-      <p>This is a tutorial page included in this Vue.js starter project, it has a few useful links and example snippets to help you get started. You can access this page at <a href="http://localhost:8080/tutorial">http://localhost:8080/tutorial</a>.</p>
+      <p>
+        This is a tutorial page included in this Vue.js starter project, it has a few useful links and example snippets to help you get started.<br>
+        You can access this page at <a href="http://localhost:8080/tutorial">http://localhost:8080/tutorial</a>.
+      </p>
       <h2>Follow these steps:</h2>
 
       <h3 id="bootstrap"><span>1</span>Bootstrap your project</h3>
-      <h4>Create a prismic.io content repository</h4>
-      <p>A repository is where your website’s content will live. Simply <a href="https://prismic.io/#create" target="_blank">create one</a> choosing a repository name and a plan. We’ve got a variety of plans including our favorite, Free!</p>
+      <h4>Create a Prismic content repository</h4>
+      <p>A repository is where your website’s content will live. Simply <a href="https://prismic.io/#create" target="_blank">create one</a> choosing a repository name and a plan. We’ve got a variety of plans including our favorite: Free!</p>
       <h4>Configure your project</h4>
-      <p>Open config.php and assign the API endpoint for your prismic.io repository to the PRISMIC_URL constant:</p>
+      <p>Open the index.html file and define the API endpoint of your Prismic repository:</p>
       <div class="source-code">
-        <pre><code>// In config.php
-define("PRISMIC_URL", "https://your-repo-name.prismic.io/api/v2");
+        <pre><code>// In index.html
+&lt;script&gt;
+  window.prismic = {
+    endpoint: 'https://your-repo-name.prismic.io/api/v2'
+  };
+&lt;/script&gt;
 </code></pre>
       </div>
-      <p>Next let's see how to create a page in your website filled with content retrieved from prismic.io!</p>
+      <p>Next let’s see how to create a Vue component filled with content retrieved from Prismic!</p>
 
       <h3 id="custom-type"><span>2</span>Create a Custom Type "Page"</h3>
-      <p>We will create a page containing a title, a paragraph and an image. Let's create a Custom Type in prismic.io with the corresponding fields. We'll add an additional UID (unique identifier) field for querying the page.</p>
-      <p>Go to the repository backend you've just created (at https://your-repo-name.prismic.io). Then navigate to the <em>"Custom Types"</em> section (icon on the left navbar) and create a new Repeatable Type, for this tutorial let's name it "Page". Make sure that the system automatically assigns this an API ID of "page".</p>
-      <p>Once the "Page" Custom Type is created, we have to define how we want to model it, that is to say a document containing a UID, a title, a paragraph and an image. Click on <em>"JSON editor"</em> (right sidebar) and paste the following JSON data into the Custom Type JSON editor. When you're done, hit <em>"Save"</em>.</p>
+      <p>We will create a page containing a title, a paragraph and an image. Let’s create a Custom Type in Prismic with the corresponding fields. We’ll add an additional UID (unique identifier) field for querying the page.</p>
+      <p>Go to the repository backend you’ve just created (at https://your-repo-name.prismic.io). Then navigate to the <em>"Custom Types"</em> section (icon on the left navbar) and create a new Repeatable Type, for this tutorial let’s name it "Page". Make sure that the system automatically assigns this an API ID of "page".</p>
+      <p>Once the "Page" Custom Type is created, we have to define how we want to model it, that is to say a document containing a UID, a title, a paragraph and an image. Click on <em>"JSON editor"</em> (right sidebar) and paste the following JSON data into the Custom Type JSON editor. When you’re done, hit <em>"Save"</em>.</p>
       <div class="source-code">
         <pre><code>{
   "Main" : {
@@ -81,16 +88,16 @@ define("PRISMIC_URL", "https://your-repo-name.prismic.io/api/v2");
       </div>
 
       <h3 id="new-page"><span>3</span>Create your first page!</h3>
-      <p>The "Page" Custom Type you've just created contains a title, a paragraph, an image and a UID (unique identifier). Now it is time to fill in your first page!</p>
+      <p>The "Page" Custom Type you’ve just created contains a title, a paragraph, an image and a UID (unique identifier). Now it is time to fill in your first page!</p>
       <p>Create a new "Page" content in your repository: go to <em>"Content"</em> and hit <em>"New"</em>.</p>
-      <p>Fill the corresponding fields. Note the value you filled in the UID field, because it will be a part of the page URL, for that purpose let's type "<strong>quickstart</strong>".</p>
-      <p>When you're done, hit <em>"Save"</em> then <em>"Publish"</em>.</p>
+      <p>Fill the corresponding fields. Note the value you filled in the UID field, because it will be a part of the page URL, for that purpose let’s type "<strong>quickstart</strong>".</p>
+      <p>When you’re done, hit <em>"Save"</em> then <em>"Publish"</em>.</p>
 
       <h3 id="code"><span>4</span>Query the API and create the page template</h3>
       <h4>Query the API for your "quickstart" page</h4>
-      <p>Now that you've created your "quickstart" page in your prismic.io repository, go back to your local code. Let's make an API call to retrieve page content. For that, we will use the specified UID.</p>
+      <p>Now that you’ve created your "quickstart" page in your Prismic repository, go back to your local code. Let’s make an API call to retrieve page content. For that, we will use the specified UID.</p>
       <p>
-        Once we've retrieved the page, we render the template providing it with its content.
+        Once we’ve retrieved the page, we render the template providing it with its content.
         <br>
         Add the following route to your app/app.php file:
       </p>
@@ -109,7 +116,7 @@ $app->get('/page/{uid}', function ($request, $response, $args) use ($app, $prism
 </code></pre>
       </div>
       <h4>Create webpage with the retrieving content</h4>
-      <p>Now all that's left to be done is to output on a webpage the content we fetched from the API. Create a new template file named "page.php" inside the views folder. Here's an example that'll display a webpage "Page" with its title, description and image:</p>
+      <p>Now all that’s left to be done is to output on a webpage the content we fetched from the API. Create a new template file named "page.php" inside the views folder. Here’s an example that’ll display a webpage "Page" with its title, description and image:</p>
       <div class="source-code">
         <pre><code>&lt;!-- Create file app/views/page.php --&gt;
 
@@ -132,12 +139,12 @@ $document = $WPGLOBAL['document'];
 &lt;?php include_once 'footer.php'; ?&gt;
 </code></pre>
       </div>
-      <p>In your browser go to <a href="http://localhost:8080/page/quickstart">http://localhost:8080/page/quickstart</a> and you're done! You've officially created a page that pulls content from prismic.io.</p>
-      <p>Obviously, since this approach is based on API, you're completely free to choose a different framework or template engine. It's all up to you!</p>
+      <p>In your browser go to <a href="http://localhost:8080/page/quickstart">http://localhost:8080/page/quickstart</a> and you’re done! You’ve officially created a page that pulls content from Prismic.</p>
+      <p>Obviously, since this approach is based on API, you’re completely free to choose a different framework or template engine. It’s all up to you!</p>
 
       <h3 id="done"><span>5</span>Well done!</h3>
       <p>Sit back and enjoy the result.</p>
-      <p>Basically in these few steps you've added content management to your page and thanks to Prismic, you'll have:</p>
+      <p>Basically in these few steps you’ve added content management to your Vue.js project and thanks to Prismic, you’ll have:</p>
       <ol style="list-style-type: disc; padding-left: 20px;">
         <li>Full versioning of your content</li>
         <li>A nice rich editor to create and edit your content</li>
