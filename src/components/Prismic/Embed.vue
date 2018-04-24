9809219e04c6d5632fa6010e7a08e3a7d4833531
jbdebiasio <jbaptistedebiasio@gmail.com>
2018-04-24 17:51:42 +0200

    create PrismicEmbed component

---
+++ src/components/Prismic/Embed.vue
@@ -0,0 +1,29 @@
+<template>
+  <component :is="EmbedComponent"/>
+</template>
+
+<script>
+export default {
+  name: 'PrismicEmbed',
+  props: {
+    field: {
+      required: true
+    }
+  },
+  computed: {
+    EmbedComponent () {
+      if (!this.field) {
+        return null;
+      }
+
+      return {
+        template: `
+          <div>
+            ${this.field.html}
+          </div>
+        `
+      };
+    }
+  }
+};
+</script>
