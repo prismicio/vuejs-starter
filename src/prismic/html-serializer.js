4338e0303b29adbaf44546150f3ccf5d6ecd85d8
jbdebiasio <jbaptistedebiasio@gmail.com>
2018-05-03 17:21:42 +0200

    handle link on image case in default html serializer

--- src/prismic/html-serializer.js
+++ src/prismic/html-serializer.js
@@ -12,9 +12,26 @@ export default function (type, element, content, children) {
       result = `<router-link to="${url}">${content}</router-link>`;
     } else {
       const target = element.data.target ? `target="'${element.data.target}'" rel="noopener"` : '';
-
       result = `<a href="${url}" ${target}>${content}</a>`;
     }
+    return result;
+  }
+
+  if (type === Elements.image) {
+    let result = `<img src="${element.url}" alt="${element.alt || ''}" copyright="${element.copyright || ''}">`;
+
+    if (element.linkTo) {
+      const url = prismicDOM.Link.url(element.linkTo, linkResolver);
+
+      if (element.linkTo.link_type === 'Document') {
+        result = `<router-link to="${url}">${result}</router-link>`;
+      } else {
+        const target = element.linkTo.target ? `target="${element.linkTo.target}" rel="noopener"` : '';
+        result = `<a href="${url}" ${target}>${result}</a>`;
+      }
+    }
+    const wrapperClassList = [element.label || '', 'block-img'];
+    result = `<p class="${wrapperClassList.join(' ')}">${result}</p>`;
     return result;
   }
 
