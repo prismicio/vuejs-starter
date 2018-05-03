eb980e8a2bdcc38b0bddf4463468cdf83f5324a5
jbdebiasio <jbaptistedebiasio@gmail.com>
2018-05-03 15:14:59 +0200

    add oembed data attributes in surrounding div of Embed component

--- src/components/Prismic/Embed.vue
+++ src/components/Prismic/Embed.vue
@@ -16,9 +16,13 @@ export default {
         return null;
       }
 
+      const urlAttr = this.field.embed_url ? `data-oembed="${this.field.embed_url}"` : '';
+      const typeAttr = this.field.type ? `data-oembed-type="${this.field.type}"` : '';
+      const providerNameAttr = this.field.provider_name ? `data-oembed-provider="${this.field.provider_name}"` : '';
+
       return {
         template: `
-          <div>
+          <div ${urlAttr} ${typeAttr} ${providerNameAttr}>
             ${this.field.html}
           </div>
         `
